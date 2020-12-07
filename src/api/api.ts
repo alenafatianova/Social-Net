bbb5254822d2ab3b26f6356f75dc47382c380ab3
Alena Fatianova <ravemybody@gmail.com>
2020-12-07 21:46:00 +0500

    api.ts small refactoring

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,16 +1,16 @@
 import axios from 'axios'
 
+const baseURL = 'https://social-network.samuraijs.com/api/1.0/'
 
 export const getUsers = (currentPage: number, pageSize: number) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}
-    &count=${pageSize}`, {
+    return axios.get(baseURL + `users?page=${currentPage} &count=${pageSize}`, {
             withCredentials: true
         })
         .then(response => response.data)
 }
 
 export const followUser = (id: number) => {
-    return axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {}, {
+    return axios.post(baseURL + `follow/${id}`, {}, {
             withCredentials: true,
             headers: {
                 'API-KEY': 'd6da0b4d-b16d-42bc-a142-317d3b9eca82'
@@ -20,7 +20,7 @@ export const followUser = (id: number) => {
 }
 
 export const deleteUser = (id: number) => {
-    return axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {
+    return axios.delete(baseURL + `follow/${id}`, {
             withCredentials: true,
             headers: {
                 'API-KEY': 'd6da0b4d-b16d-42bc-a142-317d3b9eca82'
