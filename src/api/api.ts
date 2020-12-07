e80f070f5c09fd973870fbca98a0b377524ddb3e
Alena Fatianova <ravemybody@gmail.com>
2020-12-07 21:56:05 +0500

    create instance in api

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,29 +2,24 @@ import axios from 'axios'
 
 const baseURL = 'https://social-network.samuraijs.com/api/1.0/'
 
+const instance = axios.create({
+    withCredentials: true,
+    headers: {
+        'API-KEY': 'd6da0b4d-b16d-42bc-a142-317d3b9eca82'
+    }
+})
+
 export const getUsers = (currentPage: number, pageSize: number) => {
-    return axios.get(baseURL + `users?page=${currentPage} &count=${pageSize}`, {
-            withCredentials: true
-        })
+    return instance.get(baseURL + `users?page=${currentPage} &count=${pageSize}`)
         .then(response => response.data)
 }
 
 export const followUser = (id: number) => {
-    return axios.post(baseURL + `follow/${id}`, {}, {
-            withCredentials: true,
-            headers: {
-                'API-KEY': 'd6da0b4d-b16d-42bc-a142-317d3b9eca82'
-            }
-        })
+    return instance.post(baseURL + `follow/${id}`, {})
         .then(response => response.data)
 }
 
 export const deleteUser = (id: number) => {
-    return axios.delete(baseURL + `follow/${id}`, {
-            withCredentials: true,
-            headers: {
-                'API-KEY': 'd6da0b4d-b16d-42bc-a142-317d3b9eca82'
-            }
-        })
+    return axios.delete(baseURL + `follow/${id}`)
         .then(response => response.data)
 }
