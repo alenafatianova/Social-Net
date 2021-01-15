04ff342f84b5d59ee72da6a715f5061d2aa1dba9
Alena Fatianova <ravemybody@gmail.com>
2021-01-15 18:12:41 +0100

    install redux-form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,25 +1,32 @@
 import React from 'react'
 
+export const LoginForm = () => {
+   return (
+    <form>
+    <div>
+        <input placeholder='login'/>
+    </div>
+    <div>
+        <input placeholder='password'/>
+    </div>
+    <div>
+        <input type='checkbox'/> remember me
+     </div>
+    <div>
+        <button>login</button>
+    </div>
+</form>
+   )
+}
+
 export function LoginPage() {
     return (
         <div>
            <div>
                <h1>Login</h1>
-               <form>
-                   <div>
-                       <input placeholder='login'/>
-                   </div>
-                   <div>
-                       <input placeholder='password'/>
-                   </div>
-                   <div>
-                       <input type='checkbox'/> remember me
-                   </div>
-                   <div>
-                       <button>login</button>
-                   </div>
-               </form>
+                 <LoginForm/>
             </div> 
         </div>
     )
 }
+
