ca480327f11a1f0d62e1fc7dd5f3891022c72962
Alena Fatianova <ravemybody@gmail.com>
2021-03-12 22:14:08 +0500

    create User component

---
+++ src/components/Users/User.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { UsersType } from '../../redux/users-reducer'
+import style from './Users.module.css'
+import userAvatar from '../../assets/images/userAvatar.jpg'
+
+type userTypeProps = {
+    user: UsersType
+    followingInProgress: number[]
+    unfollowUser: (id: number) => void
+    followUser: (id: number) => void
+}
+
+export const User: React.FC<userTypeProps> = ({user, followingInProgress, unfollowUser, followUser}) => {
+    return (
+        <div>
+           <span>
+                <div>
+                    <NavLink to={'/profile/' + user.id}>
+                        <img alt="user-avatar" className={style.img} src={user.photos.small != null ? user.photos.small : userAvatar}/>
+                    </NavLink>
+                        </div>
+                            <div>
+                                {
+                                    user.followed 
+                                    ? <button disabled={followingInProgress.some(id => id === user.id)} 
+                                        onClick = {() => {unfollowUser(user.id)}}>Delete</button>
+                                    : <button disabled={followingInProgress.some(id => id === user.id)} 
+                                        onClick={() => {followUser(user.id)}}>Add</button> 
+                                }
+                            </div>
+                </span>
+                    <span>
+                        <div>
+                            <div>{user.name}</div>
+                            <div>{user.status}</div>
+                        </div>
+                            <div>{'u.location.country'}</div>
+                            <div>{'u.location.city'}</div>
+                    </span>
+            </div>
+    )
+}
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import userAvatar from '../../assets/images/userAvatar.jpg'
-import style from './Users.module.css'
 import { UsersType } from '../../redux/users-reducer'
-import { NavLink } from 'react-router-dom'
 import { Paginator } from '../common/Paginator/Paginator'
+import { User } from './User'
 
 
 
@@ -37,34 +35,16 @@ export const Users = React.memo((props: UsersPropsType) => {
                 totalUsersCount={props.totalUsersCount}
                 />
                 {
-                props.users.map(u => <div key={u.id}>
-                        <span>
-                            <div>
-                                <NavLink to={'/profile/' + u.id}>
-                                <img alt="user-avatar" className={style.img} src={u.photos.small != null ? u.photos.small : userAvatar}/>
-                                </NavLink>
-                            </div>
-                            <div>
-                                {
-                                    u.followed 
-                                    ? <button disabled={props.followingInProgress.some(id => id === u.id)} 
-                                        onClick = {() => {props.unfollowUser(u.id)}}>Delete</button>
-                                    : <button disabled={props.followingInProgress.some(id => id === u.id)} 
-                                        onClick={() => {props.followUser(u.id)}}>Add</button> 
-                                }
-                            </div>
-                        </span>
-                        <span>
-                            <div>
-                                <div>{u.name}</div>
-                                <div>{u.status}</div>
-                            </div>
-                            <div>{'u.location.country'}</div>
-                            <div>{'u.location.city'}</div>
-                        </span>
-                    </div>
+                props.users.map(u => <User 
+                        key={u.id}  
+                        followingInProgress={props.followingInProgress} 
+                        unfollowUser={props.unfollowUser}
+                        followUser={props.followUser}
+                        user={u}
+                        />
+                       
                     )
                 }
-            </div>
+             </div>
     )
 })
